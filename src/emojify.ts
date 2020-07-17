54006c488dbe274ced5975aa4a8ec057fb40402d
Matthew Miller <mnmiller1@me.com>
2020-07-17 21:15:01 +1000

    feat: release 1.2.0, with multimoji support

--- src/emojify.ts
+++ src/emojify.ts
@@ -1,4 +1,8 @@
+import {lib} from 'emojilib';
 import translate from 'moji-translate';
+import * as emojiOverridesJson from './overrides.json';
+
+const emojiOverrides = emojiOverridesJson as {[key: string]: string[]};
 
 interface EmojifyOptions {
   replace?: boolean;
@@ -6,6 +10,7 @@ interface EmojifyOptions {
   replaceChance?: number;
   tripleCooldown?: number;
   tripleChance?: number;
+  multimojiChance?: number;
 }
 
 export function emojify(
@@ -15,29 +20,49 @@ export function emojify(
     replaceChance = 1,
     tripleCooldown = 3,
     tripleChance = 0.05,
+    multimojiChance = 0.1,
   }: EmojifyOptions = {}
 ): string {
   const lines = inputText.split(/\n/);
   const outputLines = [];
   const missingWords = [];
+
   for (const line of lines) {
     const words = line.split(' ');
     let lastTriple = 99999;
+
     for (const i in words) {
       const word = words[i];
       const cleanedWord = cleanWord(word);
-      let emoji = emojifyWord(cleanedWord);
-      if (emoji) {
+      let emojis = findEmojisForWord(cleanedWord);
+
+      if (emojis.length > 0) {
+        function grabEmoji(): string {
+          const emojiIndex = Math.floor(Math.random() * emojis.length);
+          const emoji = emojis[emojiIndex];
+          emojis.splice(emojiIndex, 1);
+          return emoji;
+        }
+
+        const emoji = grabEmoji();
+
+        let insert = emoji;
+
+        while (Math.random() < multimojiChance && emojis.length > 1) {
+          insert += grabEmoji();
+        }
+
         if (Math.random() < tripleChance && lastTriple > tripleCooldown) {
-          emoji = emoji + emoji + emoji;
+          insert = insert + insert + insert;
           lastTriple = 0;
         } else {
           lastTriple++;
         }
+
         if (replace && Math.random() < replaceChance) {
-          words[i] = emoji;
+          words[i] = insert;
         } else {
-          words[i] = word + ' ' + emoji + ' ';
+          words[i] = word + ' ' + insert + ' ';
         }
       } else {
         missingWords.push(cleanedWord);
@@ -61,45 +86,76 @@ function cleanWord(word: string): string {
   return word.toLowerCase();
 }
 
-function emojifyWord(word: string): string | undefined {
-  let emoji = translate.getEmojiForWord(word);
-  if (!emoji) {
-    // Do some custom overrides
-    if (word.endsWith("'s")) {
-      const e = translate.getEmojiForWord(word.substring(0, word.length - 2));
-      if (e) {
-        return e;
-      }
-    }
-    if (word.endsWith("'")) {
-      const e = translate.getEmojiForWord(
-        word.substring(0, word.length - 1) + 'g'
-      );
-      if (e) {
-        return e;
-      }
-    }
-    if (word.includes('-')) {
-      let modifiedWord = word;
-      while (modifiedWord.includes('-')) {
-        modifiedWord = modifiedWord.split('-', 2)[1];
-        const e = translate.getEmojiForWord(modifiedWord);
-        if (e) {
-          return e;
-        }
-      }
-    }
+function findEmojisForWord0(word: string): string[] {
+  const foundEmojis: string[] = [];
 
-    // Specific words
-    if (word === 'yell' || word === 'yelling') {
-      return '🤬';
-    }
-    if (word === 'tell') {
-      return '🗣';
+  if (word === 'a' || word === 'is' || word === 'it') {
+    return foundEmojis;
+  }
+
+  if (translate.isMaybeAlreadyAnEmoji(word)) {
+    return foundEmojis;
+  }
+
+  if (emojiOverrides[word]) {
+    foundEmojis.push(...emojiOverrides[word]);
+  }
+
+  for (let emoji in lib) {
+    if (
+      word === emoji ||
+      word + '_face' === emoji ||
+      word === lib[emoji].char ||
+      word === lib[emoji].category ||
+      lib[emoji].keywords.includes(word)
+    ) {
+      if (word.length < 3 && lib[emoji].category === 'flags') {
+        // Ignore tiny flags.
+        continue;
+      }
+      foundEmojis.push(lib[emoji].char);
     }
   }
-  if (word === emoji) {
-    return undefined;
+
+  return foundEmojis;
+}
+
+const emojiModFunctions: ((word: string) => string | undefined)[] = [
+  (w) => w,
+  (w) => (w.length > 1 ? `${w}s` : undefined),
+  (w) => (w.length > 1 ? `${w}ing` : undefined),
+  (w) => (w.length > 1 ? `${w}ed` : undefined),
+  (w) => (w.endsWith("'s") ? w.substring(0, w.length - 2) : undefined),
+  (w) => (w.endsWith("'") ? `${w.substring(0, w.length - 1)}g` : undefined),
+  (w) =>
+    w.length > 2 && w.endsWith('s') ? w.substring(0, w.length - 1) : undefined,
+  (w) => (w.endsWith('ing') ? w.substring(0, w.length - 3) : undefined),
+  (w) => (w.endsWith('ing') ? `${w.substring(0, w.length - 3)}e` : undefined),
+  (w) =>
+    w.length > 5 && w.endsWith('ing')
+      ? w.substring(0, w.length - 4)
+      : undefined,
+];
+
+function findEmojisForWord(word: string): string[] {
+  const foundEmojis: string[] = [];
+
+  // Try the word, and various permutations.
+  word = word.trim().toLowerCase();
+
+  // Split hyphenated words
+  if (word.includes('-')) {
+    word
+      .split('-')
+      .map(findEmojisForWord)
+      .forEach((words) => foundEmojis.push(...words));
   }
-  return emoji;
+
+  // Run it for each functor
+  emojiModFunctions
+    .map((func) => func(word))
+    .filter((w) => w)
+    .forEach((w) => foundEmojis.push(...findEmojisForWord0(w!)));
+
+  return foundEmojis;
 }
--- typings.d.ts
+++ typings.d.ts
@@ -1,1 +1,15 @@
-declare module 'moji-translate';
+declare module 'moji-translate' {
+    export function getEmojiForWord(word: string): string;
+
+    export function isMaybeAlreadyAnEmoji(word: string): boolean;
+}
+
+declare module 'emojilib' {
+    export interface EmojiLibEntry {
+        keywords: string[];
+        char: string;
+        category: string;
+    }
+
+    export const lib: {[key: string]: EmojiLibEntry};
+}
