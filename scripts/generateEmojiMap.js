852bd18da35b9ee52b751e5a1c02e7bd40172b4d
Matthew Miller <mnmiller1@me.com>
2020-11-22 20:41:01 +1000

    release(1.3.0): release 1.3.0 with performance boosts

---
+++ scripts/generateEmojiMap.js
@@ -0,0 +1,27 @@
+const { lib } = require('emojilib');
+const fs = require('fs-extra');
+
+async function main() {
+    const emojiData = Object.keys(lib).reduce((obj, emojiKey) => {
+        const emoji = lib[emojiKey];
+        obj[emoji.char] = emoji.keywords.reduce((a, b) => {
+            a.push(...b.split('-'));
+            return a;
+        }, []);
+        obj[emoji.char].push(emojiKey);
+        obj[emoji.char].push(...emojiKey.split('_'));
+        obj[emoji.char] = obj[emoji.char].filter(
+            w =>
+                w !== 'and' &&
+                w !== 'in' &&
+                w !== 'with' &&
+                w !== 'of' &&
+                w !== 'a'
+        );
+        return obj;
+    }, {});
+
+    await fs.writeFile(`dist/emojiMap.json`, JSON.stringify(emojiData));
+}
+
+main().catch(e => console.error(e));
